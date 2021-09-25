06e6a9d38c66ae1a18ceec320f6a6bbfcd60e451
Khang Lam <khangvippro357@gmail.com>
2021-09-25 17:06:58 +0700

    JS darkmode and fix number float of battery + css battery

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let percentage = document.querySelector('.percentage')
-let percent = document.querySelector('.percent');
+let percent = document.querySelector('.percent')
 
 // Get capacity battery from computer
 navigator.getBattery().then((battery) => {
-    percentage.style.width = battery.level*100 + '%';
-    percent.innerHTML = battery.level*100 + '%';
+  percentage.style.width = battery.level * 100 + '%'
+  percent.innerHTML = parseInt(battery.level * 100) + '%'
+})
 
+let sec = document.querySelector('.sec')
+let toggle = document.querySelector('.toggle')
+// toggle.onclick = () => {
+//     sec.classList.toggle('dark')
+
+// }
+
+toggle.addEventListener('click', () => {
+  sec.classList.toggle('dark')
 })
